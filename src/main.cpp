384a1e4b4409ce9bcaa58d7f3e76b31b5a56599d
Charles Dorval (deskwizard) <deskwizard@gmail.com>
2022-03-16 09:30:45 -0400

    serial stuff

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,9 @@
 
 void initOTA();
 void handleOTA();
+void initSerial();
+void handleSerial();
+void printSerialHelp();
 
 #ifdef BT_SERIAL_ENABLED
 BluetoothSerial SerialBT;
@@ -17,23 +20,13 @@ void printSerialHelp();
 float CtoF(float celsius) { return (celsius * 1.8f) + 32.0f; }
 
 void setup() {
-  // Figures at 5V input, measured with the shitty USB meter
-  // using 5V the USB-UART adapter is powered....
-  // We need at least 80MHz for WiFi, so set it back when we enable OTA.
+  
   // setCpuFrequencyMhz(160); //  70mA
-
-  Serial.begin(115200);
-  delay(2000);
-  Serial.println("Hello");
-  Serial.println();
-#ifdef BT_SERIAL_ENABLED
-  SerialBT.begin("Reflow Oven"); // Bluetooth device name
-  SerialBT.println("Hello");
-  SerialBT.println();
-#endif
-
+  
+  initSerial();
   initKeypad();
   initDisplay();
+
   if (getDeviceMode() == MODE_OTA) {
     // setCpuFrequencyMhz(80);
     initOTA();
@@ -53,114 +46,129 @@ void loop() {
     handleKeypad();
     handleSensor();
     handlePID();
-
-    if (Serial.available()) {
-
-      unsigned char c = Serial.read();
-      float readValueF;
-      uint16_t readValueC;
-
-      switch (c) {
-
-      case 'p':
-        readValueF = Serial.parseFloat();
-        // setKp(readValueF);
-        setNearKp(readValueF);
-        break;
-      case 'i':
-        readValueF = Serial.parseFloat();
-        // setKi(readValueF);
-        setNearKi(readValueF);
-        break;
-      case 'd':
-        readValueF = Serial.parseFloat();
-        // setKd(readValueF);
-        setNearKd(readValueF);
-        break;
-
-      case 'P':
-        Serial.print("Kp: ");
-        Serial.println(getKp(), 4);
-        break;
-      case 'I':
-        Serial.print("Ki: ");
-        Serial.println(getKi(), 4);
-        break;
-      case 'D':
-        Serial.print("Kd: ");
-        Serial.println(getKd(), 4);
-        break;
-
-      case 'S':
-        startPID();
-        break;
-
-      case 's':
-        stopPID();
-        break;
-
-      case 'c':
-        readValueC = Serial.parseInt();
-        setPreheatDutyCycle(readValueC);
-        break;
-
-      case 'v':
-        readValueC = Serial.parseInt();
-        setPreheatTime(readValueC);
-        break;
-
-      case 'w':
-        readValueC = Serial.parseInt();
-        setDwellTime(readValueC);
-        break;
-
-      case 'C':
-        Serial.print("Prehead Duty cycle: ");
-        Serial.println(getPreheatDutyCycle());
-        Serial.print("/");
-        Serial.println(MAX_DUTY_CYCLE);
-        break;
-
-      case 'V':
-        Serial.print("Preheat time: ");
-        Serial.println(getPreheatTime());
-        break;
-
-      case 'W':
-        Serial.print("Dwell time: ");
-        Serial.println(getDwellTime());
-        break;
-
-      case 'Y':
-        Serial.print("Preheat percent: ");
-        Serial.println(getPreheatPercent());
-        break;
-
-      case 'y':
-        readValueC = Serial.parseInt();
-        setPreheatPercent(readValueC);
-        break;
-
-      case 'h':
-        printSerialHelp();
-        break;
-
-      case 'f':
-        readValueC = Serial.parseInt();
-        if (readValueC == 20 || readValueC == 40 || readValueC == 80 ||
-            readValueC == 160) {
-          setCpuFrequencyMhz(readValueC);
-          Serial.print("F_CPU: ");
-          Serial.println(readValueC);
-        } else {
-          Serial.println("Invalid FCPU");
-        }
-        break;
-
-      } // switch
-    }
+    handleSerial();
   } else {
     handleOTA();
+  }
+}
+
+void initSerial() {
+  Serial.begin(115200);
+  delay(2000);
+  Serial.println("Hello");
+  Serial.println();
+#ifdef BT_SERIAL_ENABLED
+  SerialBT.begin("Reflow Oven"); // Bluetooth device name
+  SerialBT.println("Hello");
+  SerialBT.println();
+#endif
+}
+
+void handleSerial() {
+  if (Serial.available()) {
+
+    unsigned char c = Serial.read();
+    float readValueF;
+    uint16_t readValueC;
+
+    switch (c) {
+
+    case 'p':
+      readValueF = Serial.parseFloat();
+      // setKp(readValueF);
+      setNearKp(readValueF);
+      break;
+    case 'i':
+      readValueF = Serial.parseFloat();
+      // setKi(readValueF);
+      setNearKi(readValueF);
+      break;
+    case 'd':
+      readValueF = Serial.parseFloat();
+      // setKd(readValueF);
+      setNearKd(readValueF);
+      break;
+
+    case 'P':
+      Serial.print("Kp: ");
+      Serial.println(getKp(), 4);
+      break;
+    case 'I':
+      Serial.print("Ki: ");
+      Serial.println(getKi(), 4);
+      break;
+    case 'D':
+      Serial.print("Kd: ");
+      Serial.println(getKd(), 4);
+      break;
+
+    case 'S':
+      startPID();
+      break;
+
+    case 's':
+      stopPID();
+      break;
+
+    case 'c':
+      readValueC = Serial.parseInt();
+      setPreheatDutyCycle(readValueC);
+      break;
+
+    case 'v':
+      readValueC = Serial.parseInt();
+      setPreheatTime(readValueC);
+      break;
+
+    case 'w':
+      readValueC = Serial.parseInt();
+      setDwellTime(readValueC);
+      break;
+
+    case 'C':
+      Serial.print("Prehead Duty cycle: ");
+      Serial.println(getPreheatDutyCycle());
+      Serial.print("/");
+      Serial.println(MAX_DUTY_CYCLE);
+      break;
+
+    case 'V':
+      Serial.print("Preheat time: ");
+      Serial.println(getPreheatTime());
+      break;
+
+    case 'W':
+      Serial.print("Dwell time: ");
+      Serial.println(getDwellTime());
+      break;
+
+    case 'Y':
+      Serial.print("Preheat percent: ");
+      Serial.println(getPreheatPercent());
+      break;
+
+    case 'y':
+      readValueC = Serial.parseInt();
+      setPreheatPercent(readValueC);
+      break;
+
+    case 'h':
+      printSerialHelp();
+      break;
+
+    case 'f':
+      readValueC = Serial.parseInt();
+      if (readValueC == 20 || readValueC == 40 || readValueC == 80 ||
+          readValueC == 160) {
+        setCpuFrequencyMhz(readValueC);
+        Serial.print("F_CPU: ");
+        Serial.println(readValueC);
+      } else {
+        Serial.println("Invalid FCPU");
+      }
+      break;
+
+    } // switch
   }
 }
 
